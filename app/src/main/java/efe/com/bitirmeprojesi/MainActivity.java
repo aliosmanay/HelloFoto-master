f1909c80d29bbce12aa1995ea3fbd6576cba8a99
aliosmanay <a.o.a1618@gmail.com>
2018-01-21 17:57:29 +0300

    proje yüklendi

---
+++ app/src/main/java/efe/com/bitirmeprojesi/MainActivity.java
@@ -0,0 +1,13 @@
+package efe.com.bitirmeprojesi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
